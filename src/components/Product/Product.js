299f3b6a6a4a4b774daf31792a5b55b1e04c3d2d
Bahodir <bahodira213@gmail.com>
2021-02-14 21:54:11 +0500

    product styled

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,15 +5,15 @@ import classes from "./Product.module.css";
 const product = (props) => {
   console.log(props.product);
   return (
-    <div>
-      <div>
+    <div className={classes.product}>
+      <div className={classes.productImage}>
         <img src={props.product.imgSrc} alt={props.product.name} />
       </div>
-      <div>
+      <div className={classes.productInfo}>
         <h4>{props.product.name}</h4>
         <p>{props.product.description}</p>
-        <div>{props.product.price}</div>
-        <div>+</div>
+        <div className={classes.productPrice}>{props.product.price} UZS</div>
+        <div className={classes.addButton}>+</div>
       </div>
     </div>
   );
--- src/components/ProductHolder/ProductHolder.js
+++ src/components/ProductHolder/ProductHolder.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 import Product from "../Product/Product";
+import classes from "./ProductHolder.module.css";
 
 const productHolder = (props) => {
-//   console.log(props.products);
+  //   console.log(props.products);
   const products = props.products.map((product) => {
     return <Product key={product.name} product={product} />;
   });
-  return <div>{products}</div>;
+  return <div className={classes.productHolder}>{products}</div>;
 };
 
 export default productHolder;
