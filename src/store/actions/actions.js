8f6de9e29ec60e6b7d9d7a73e204c150912fb8a8
Bahodir <bahodira213@gmail.com>
2021-02-19 17:01:16 +0500

    getting product info from firebase

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -40,160 +40,30 @@ export const fetchProductsFailed = () => {
     type: actionTypes.FETCH_PRODUCTS_FAILED,
   };
 };
-const response = {
-  data: {
-    products: [
-      {
-        name: "Gavaya",
-        description:
-          "Горячая закуска с митболами из говядины, томатами, моцареллой и соусом чипотле",
-        price: 45000,
-        imgSrc: images.Gavaya,
-        type: "pizza",
-      },
-      {
-        name: "Mexica",
-        description:
-          "Горячая закуска с митболами из говядины, томатами, моцареллой и соусом чипотле",
-        price: 53000,
-        imgSrc: images.Mexica,
-        type: "pizza",
-      },
-      {
-        name: "Hot achchiko",
-        description:
-          "Горячая закуска с митболами из говядины, томатами, моцареллой и соусом чипотле",
-        price: 64000,
-        imgSrc: images.HotAchchiko,
-        type: "pizza",
-      },
-      {
-        name: "Havai",
-        description:
-          "Горячая закуска с митболами из говядины, томатами, моцареллой и соусом чипотле",
-        price: 45000,
-        imgSrc: images.Havai,
-        type: "pizza",
-      },
-      {
-        name: "Apocalipo",
-        description:
-          "Горячая закуска с митболами из говядины, томатами, моцареллой и соусом чипотле",
-        price: 64000,
-        imgSrc: images.Apocalipo,
-        type: "pizza",
-      },
-      {
-        name: "Sosikacho",
-        description:
-          "Горячая закуска с митболами из говядины, томатами, моцареллой и соусом чипотле",
-        price: 45000,
-        imgSrc: images.Sosiskacho,
-        type: "pizza",
-      },
-      {
-        name: "Mexico",
-        description:
-          "Горячая закуска с митболами из говядины, томатами, моцареллой и соусом чипотле",
-        price: 53000,
-        imgSrc: images.Mexico,
-        type: "pizza",
-      },
-      {
-        name: "Cheeseburger",
-        description:
-          "Горячая закуска с митболами из говядины, томатами, моцареллой и соусом чипотле",
-        price: 17000,
-        imgSrc: images.Cheeseburger,
-        type: "burger",
-      },
-      {
-        name: "Chili burger",
-        description:
-          "Горячая закуска с митболами из говядины, томатами, моцареллой и соусом чипотле",
-        price: 18000,
-        imgSrc: images.Chiliburger,
-        type: "burger",
-      },
-      {
-        name: "Hamburger",
-        description:
-          "Горячая закуска с митболами из говядины, томатами, моцареллой и соусом чипотле",
-        price: 15000,
-        imgSrc: images.Hamburger,
-        type: "burger",
-      },
-      {
-        name: "Double burger",
-        description:
-          "Горячая закуска с митболами из говядины, томатами, моцареллой и соусом чипотле",
-        price: 20000,
-        imgSrc: images.Doubleburger,
-        type: "burger",
-      },
-      {
-        name: "Combo-1",
-        description:
-          "Горячая закуска с митболами из говядины, томатами, моцареллой и соусом чипотле",
-        price: 25000,
-        imgSrc: images.Combo1,
-        type: "combo",
-      },
-      {
-        name: "Combo-2",
-        description:
-          "Горячая закуска с митболами из говядины, томатами, моцареллой и соусом чипотле",
-        price: 30000,
-        imgSrc: images.Combo2,
-        type: "combo",
-      },
-      {
-        name: "Combo-3",
-        description:
-          "Горячая закуска с митболами из говядины, томатами, моцареллой и соусом чипотле",
-        price: 28000,
-        imgSrc: images.Combo3,
-        type: "combo",
-      },
-      {
-        name: "Sprite 1L",
-        description:
-          "Горячая закуска с митболами из говядины, томатами, моцареллой и соусом чипотле",
-        price: 6000,
-        imgSrc: images.Sprite,
-        type: "drink",
-      },
-      {
-        name: "Coca-cola 1.5L",
-        description:
-          "Горячая закуска с митболами из говядины, томатами, моцареллой и соусом чипотле",
-        price: 9000,
-        imgSrc: images.CocaCola,
-        type: "drink",
-      },
-      {
-        name: "Fanta 1L",
-        description:
-          "Горячая закуска с митболами из говядины, томатами, моцареллой и соусом чипотле",
-        price: 6000,
-        imgSrc: images.Fanta,
-        type: "drink",
-      },
-    ],
-  },
-};
 
 //Reaching out the web to get all product types we have
 export const initProducts = () => {
   return (dispatch) => {
-    // axios
-    //   .get("url")
-    //   .then((response) => {
-    dispatch(setProducts(response.data.products)); // Setting gotten products to our state
-    dispatch(initPurchasedProducts(response.data.products));
-    //   })
-    //   .catch((error) => {
-    //     dispatch(fetchProductsFailed()); // When we can't get products
-    //   });
+    axios.post(
+      "https://maxway-e7045-default-rtdb.firebaseio.com/products.json",
+      {
+        name: "Combo-1",
+        description:
+          "Горячая закуска с митболами из говядины, томатами, моцареллой и соусом чипотле",
+        price: 30000,
+        type: "combo",
+      }
+    );
+    axios
+      .get("https://maxway-e7045-default-rtdb.firebaseio.com/products.json")
+      .then((response) => {
+        const products = Object.values(response.data);
+        dispatch(setProducts(products)); // Setting gotten products to our state
+        dispatch(initPurchasedProducts(products));
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch(fetchProductsFailed()); // When we can't get products
+      });
   };
 };
